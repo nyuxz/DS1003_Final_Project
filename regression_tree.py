d8e4d90149868ba083ab5255a29e7aa352544f8d
Sean <xz1757@nyu.edu>
2017-05-07 00:30:53 -0400

    tiny change for final run

--- regression_tree.py
+++ regression_tree.py
@@ -1,36 +1,36 @@
 import sys
 import os
-
-# Change your path
-
-os.chdir('/Users/siyang/Downloads/DS1003_Final_Project-master/')
-sys.path.append('/Users/siyang/Downloads/DS1003_Final_Project-master/')
-
-# Codes starts here
-
-# Import the necessary modules and libraries
-#from importlib import reload
-#reload(util)
 import util
 from util import regression_loss
-#from sklearn.kernel_ridge import KernelRidge
 from sklearn.model_selection import GridSearchCV
 import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.pipeline import Pipeline
 import pickle
-#import matplotlib.pyplot as plt
+
+
+# importance_threshold = 0.001 achieved best train loss
+def number_best_feature_set(read_data_datapath):
+    min_train_loss = 100
+    threshold = 0
+    for i in np.array([0.001, 0.002, 0.003, 0.004, 0.005, 0.006]):
+        x_train, x_test, y_train, y_test = util.prepare_train_test_set(read_data_datapath, i)
+        clf = xgb.XGBRegressor(seed = 2017)
+        clf.fit(x_train, y_train)
+        train_loss, test_loss = util.quick_test_model(x_train, x_test, y_train, y_test, clf, regression_loss)
+        print (train_loss, i)
+
 
 def run_model(read_data_datapath, save_model_path):
     # read data
-    x_cv, x_test, y_cv, y_test = util.prepare_train_test_set(read_data_datapath)
+    x_cv, x_test, y_cv, y_test = util.prepare_train_test_set(read_data_datapath,0.005)
     # choose model
     clf = Pipeline([('clf',DecisionTreeRegressor(criterion='mse',random_state=0))])
     #clf = DecisionTreeRegressor(criterion='mse',random_state=0)
     # grid search for the best fit parameters
     parameters = {
-        'clf__max_depth': [125, 100, 75, 50, 40, 30, 25, 20, 15, 10, 5],
-        'clf__min_samples_split': [2, 3, 4 ,5, 6],
+        #'clf__max_depth': [125, 100, 75, 50, 40, 30, 25, 20, 15, 10, 5],
+        #'clf__min_samples_split': [2, 3, 4 ,5, 6],
         'clf__min_samples_leaf': [1, 2, 3, 4 ,5, 6]  
     }
         
@@ -49,6 +49,7 @@ def run_model(read_data_datapath, save_model_path):
     #train_loss, test_loss = util.quick_test_model(x_cv[1:100,:], x_test[1:100,:], y_cv[1:100], y_test[1:100], CV_clf, regression_loss)
     print("Train loss is %s, \n Test loss is %s  " % (train_loss, test_loss))
 
+
 def show_result(read_model_path):
     # load model from file
     model = pickle.load(open(read_model_path, "rb"))
@@ -62,17 +63,13 @@ def show_result(read_model_path):
         
 if __name__ == '__main__':
     run_model('./data/encoded_entire.pkl', "./model/tree_entire_model.pkl")
-    run_model('./data/encoded_others.pkl', "./model/tree_others_model.pkl")
+    run_model('./data/encoded_private.pkl', "./model/tree_private_model.pkl")
 
     show_result("./model/tree_entire_model.pkl")
-    show_result("./model/tree_others_model.pkl")
-
+    show_result("./model/tree_private_model.pkl")
 
 # Predict
-
 #y_pred = CV_clf.predict(x_test)
-
-
 # Visualization 
 #import pydot  
 #tree.export_graphviz(dtreg, out_file='tree.dot') #produces dot file
